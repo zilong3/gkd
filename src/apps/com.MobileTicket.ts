317da6b6c2e915720929ca3eee2186d4de8577e6
zilong3 <zilong3@email>
2026-05-08 02:40:37 +0800

    fix

--- src/apps/com.MobileTicket.ts
+++ src/apps/com.MobileTicket.ts
@@ -5,18 +5,18 @@ export default defineGkdApp({
   name: '铁路12306',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '开屏广告',
       matchTime: 5000,
       actionMaximum: 1,
       rules: [
         {
-          key: 1,
+          key: 0,
           matches: ['[vid="fl_adContent_container"] >2 [vid="tv_skip"]'],
           snapshotUrls: ['https://i.gkd.li/i/27478746'],
         },
         {
-          key: 2,
+          key: 1,
           matches: ['[vid="cl_image_splash"] > [vid="tv_main_splash_skip"]'],
           snapshotUrls: ['https://i.gkd.li/i/27484445'],
         },
--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -5,23 +5,23 @@ export default defineGkdApp({
   name: '酷安',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '开屏广告',
       matchTime: 5000,
       actionMaximum: 1,
       rules: [
         {
-          key: 1,
+          key: 0,
           matches: ['@ImageView[desc=null] <<n [vid="ad_container"]'],
           snapshotUrls: ['https://i.gkd.li/i/27481905'],
         },
         {
-          key: 2,
+          key: 1,
           matches: ['@TextView[text^="跳过"] <<n [vid="ad_container"]'],
           snapshotUrls: ['https://i.gkd.li/i/27489253'],
         },
         {
-          key: 3,
+          key: 2,
           matches: ['@View[index=2] <<n [vid="main_splash_ad"]'],
           snapshotUrls: ['https://i.gkd.li/i/27492229'],
         },
